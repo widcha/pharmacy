a1746c833d0c47b88800ede0f09cc7003baffd9f
Wido Wicaksono <widowicak@gmail.com>
2021-03-16 12:43:46 +0700

    PWP-13-KeepLogin

--- front-end/src/redux/reducers/configureStore.js
+++ front-end/src/redux/reducers/configureStore.js
@@ -5,15 +5,16 @@ import logger from "redux-logger";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
+// KEEP LOGIN USING REDUX PERSIST
 import rootReducer from ".";
 const persistConfig = {
-	key: "root",
-	storage,
-	whitelist: ["user"],
+  key: "root",
+  storage,
+  whitelist: ["user"],
 };
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 export const store = configureStore({
-	reducer: persistedReducer,
-	middleware: [thunk, logger],
+  reducer: persistedReducer,
+  middleware: [thunk, logger],
 });
 export const persistor = persistStore(store);
