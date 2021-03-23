e85da07313596d9d416175866e8c999b9875db5d
Wido Wicaksono <widowicak@gmail.com>
2021-03-23 15:11:27 +0700

    Add to cart btn

--- front-end/src/redux/actions/customOrderAction.js
+++ front-end/src/redux/actions/customOrderAction.js
@@ -1,3 +1,8 @@
+import axios from "axios";
+import { api_url } from "../../helpers";
+
+const url = api_url + "/custom-product";
+
 export const addCustomProductAction = (product) => {
   return async (dispatch) => {
     console.log(product);
@@ -17,6 +22,34 @@ export const customQtyAction = (newState) => {
     try {
       dispatch({ type: "API_CUSTOM_START" });
       dispatch({ type: "CHANGE_QTY", payload: newState });
+      dispatch({ type: "API_CUSTOM_SUCCESS" });
+    } catch (err) {
+      dispatch({ type: "API_CUSTOM_FAILED", payload: err.message });
+    }
+  };
+};
+
+export const addProductToDatabaseAction = ({
+  totalQty,
+  totalPrice,
+  capsule,
+  user_id,
+}) => {
+  return async (dispatch) => {
+    console.log(capsule);
+    try {
+      dispatch({ type: "API_CUSTOM_START" });
+      const response = await axios.post(`${url}`, {
+        totalQty,
+        totalPrice,
+        capsule,
+        user_id,
+      });
+      console.log(response);
+      // dispatch({
+      //   type: "USER_FETCH_CART",
+      //   payload: response.data,
+      // });
       dispatch({ type: "API_CUSTOM_SUCCESS" });
     } catch (err) {
       dispatch({ type: "API_CUSTOM_FAILED", payload: err.message });
