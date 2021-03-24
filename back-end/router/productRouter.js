7e11a59232c45e7c8ee55417ce715f84178550de
Lani Diana <lanidianatjiam@gmail.com>
2021-03-24 20:40:13 +0700

    pisahin admin-user

--- back-end/router/productRouter.js
+++ back-end/router/productRouter.js
@@ -17,16 +17,19 @@ const express = require("express");
 const {searchProduct} = require("../controller/productController");
 const router = express.Router();
 
+//Admin
 router.get("/all-state", getDeletedProduct);
 router.get("/", getAllProduct);
+router.post("/", addNewProduct);
+router.patch("/delete/:id", deleteProduct);
+router.patch("/:id", editProduct);
+router.patch("/stock/:id", addProductStock);
+
+//User
 router.get("/by-user", getAllProductbyUser);
 router.get("/sort", sortProduct);
 router.get("/categories", getProductCategories);
 router.get("/search", searchProduct);
 router.get("/:id", getProductById);
-router.post("/", addNewProduct);
-router.patch("/delete/:id", deleteProduct);
-router.patch("/:id", editProduct);
-router.patch("/stock/:id", addProductStock);
 
 module.exports = router;
