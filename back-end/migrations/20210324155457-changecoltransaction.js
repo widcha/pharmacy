3f05bd3e5542b9993a3e6eb2419641a142f2f847
Lani Diana <lanidianatjiam@gmail.com>
2021-03-25 13:06:11 +0700

    .

--- back-end/migrations/20210324155457-changecoltransaction.js
+++ back-end/migrations/20210324155457-changecoltransaction.js
@@ -1,43 +1,52 @@
 "use strict";
 
 module.exports = {
-	up: async (queryInterface, Sequelize) => {
-		/**
-		 * Add altering commands here.
-		 *
-		 * Example:
-		 * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-		 */
-		// await queryInterface.removeColumn(
-		// 	"payment_images",
-		// 	"transaction_invoice_number"
-		// );
-		// await queryInterface.removeColumn("finances", "transaction_invoice_number");
-		// await queryInterface.removeColumn("finances", "finance_date");
-		await queryInterface.removeColumn(
-			"user_notifs",
-			"transaction_invoice_number"
-		);
-		await queryInterface.removeColumn(
-			"transactions",
-			"transaction_invoice_number"
-		);
-		await queryInterface.addColumn(
-			"transactions",
-			"transaction_invoice_number",
-			{
-				type: Sequelize.STRING(500),
-				allowNull: false,
-			}
-		);
-	},
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    // await queryInterface.removeColumn(
+    // 	"payment_images",
+    // 	"transaction_invoice_number"
+    // );
+    // await queryInterface.removeColumn("finances", "transaction_invoice_number");
+    // await queryInterface.removeColumn("finances", "finance_date");
+    // await queryInterface.removeColumn(
+    //   "admin_notifs",
+    //   "transaction_invoice_number"
+    // );
+    // await queryInterface.removeColumn(
+    //   "payment_images",
+    //   "transaction_invoice_number"
+    // );
+    // await queryInterface.removeColumn(
+    //   "user_notifs",
+    //   "transaction_invoice_number"
+    // );
+    // await queryInterface.removeColumn("finances", "transaction_invoice_number");
+    // await queryInterface.removeColumn(
+    //   "transactions",
+    //   "transaction_invoice_number"
+    // );
+    // await queryInterface.addColumn(
+    //   "transactions",
+    //   "transaction_invoice_number",
+    //   {
+    //     type: Sequelize.STRING(500),
+    //     allowNull: false,
+    //   }
+    // );
+  },
 
-	down: async (queryInterface, Sequelize) => {
-		/**
-		 * Add reverting commands here.
-		 *
-		 * Example:
-		 * await queryInterface.dropTable('users');
-		 */
-	},
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
 };
--- back-end/models/payment_images.js
+++ back-end/models/payment_images.js
@@ -1,49 +1,45 @@
 "use strict";
-const { Model } = require("sequelize");
+const {Model} = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-	class Payment_Images extends Model {
-		/**
-		 * Helper method for defining associations.
-		 * This method is not a part of DataTypes lifecycle.
-		 * The `models/index` file will call this method automatically.
-		 */
-		static associate(models) {
-			// define association here
-			this.belongsTo(models.User, {
-				foreignKey: "user_id",
-			});
-		}
-	}
-	Payment_Images.init(
-		{
-			payment_images_id: {
-				allowNull: false,
-				autoIncrement: true,
-				primaryKey: true,
-				type: DataTypes.INTEGER,
-			},
-			user_id: {
-				type: DataTypes.INTEGER,
-				references: {
-					model: "users",
-					key: "user_id",
-				},
-			},
-			payment_images_image_path: {
-				type: DataTypes.STRING,
-			},
-			transaction_invoice_number: {
-				type: DataTypes.INTEGER,
-				references: {
-					model: "transactions",
-					key: "transaction_invoice_number",
-				},
-			},
-		},
-		{
-			sequelize,
-			modelName: "Payment_Images",
-		}
-	);
-	return Payment_Images;
+  class Payment_Images extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of DataTypes lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.belongsTo(models.User, {
+        foreignKey: "user_id",
+      });
+    }
+  }
+  Payment_Images.init(
+    {
+      payment_images_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "users",
+          key: "user_id",
+        },
+      },
+      payment_images_image_path: {
+        type: DataTypes.STRING,
+      },
+      transaction_invoice_number: {
+        type: DataTypes.INTEGER(500),
+      },
+    },
+    {
+      sequelize,
+      modelName: "Payment_Images",
+    }
+  );
+  return Payment_Images;
 };
